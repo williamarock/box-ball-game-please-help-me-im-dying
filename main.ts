15dee3339f4f02899af13aeaef186f879a91c3f6
williamarock <123965023+williamarock@users.noreply.github.com>
2023-04-17 10:31:38 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,103 @@
-// When spritekind projectile overlaps with spritekind player, the velocity gets reversed on the y and random between -50 and 50 for the x
+namespace SpriteKind {
+    export const block = SpriteKind.create()
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
-    Ball.setVelocity(randint(-50, 50), -50)
+    Ball2.setVelocity(Ball2.vx, -1 * Ball2.vy)
 })
-let Ball: Sprite = null
+function spawnBlocks (num: number, block1: any[], block2: any[], block3: any[]) {
+    for (let index = 0; index <= 9; index++) {
+        for (let index2 = 0; index2 <= 2; index2++) {
+            x = index * 18
+            if (index2 % 2 == 1) {
+                x = index * 18 + 8
+            }
+            blockpick = randint(0, 2)
+            if (blockpick == 0) {
+                Block = sprites.create(img`
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+                    `, SpriteKind.block)
+            } else if (blockpick == 1) {
+                Block = sprites.create(img`
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+                    `, SpriteKind.block)
+            } else {
+                Block = sprites.create(img`
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+                    `, SpriteKind.block)
+            }
+            Block.setPosition(x, index2 * 18 + 20)
+        }
+    }
+    return info.score() + 1
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.block, function (sprite, otherSprite) {
+    findPosition(Ball2, Block)
+    info.changeScoreBy(1)
+    if (direction == 1) {
+        Ball2.setVelocity(Ball2.vx, -1 * Ball2.vy)
+    } else {
+        Ball2.setVelocity(-1 * Ball2.vx, Ball2.vy)
+    }
+    sprites.destroy(otherSprite)
+})
+function findPosition (ball: Sprite, block: Sprite) {
+    if (ball.x < block.x - 8 || ball.x > block.x + 8) {
+        direction = 1
+    } else {
+        direction = 0
+    }
+}
+let direction = 0
+let Block: Sprite = null
+let blockpick = 0
+let x = 0
+let list: number[] = []
+let Ball2: Sprite = null
 // This creates the player sprite and sets it as a player kind
 let Burtha = sprites.create(assets.image`Burtha`, SpriteKind.Player)
 // Sets the player sprite at x83 and y125
@@ -12,7 +107,7 @@ controller.moveSprite(Burtha, 100, 0)
 // Doesn't allow the player sprite to go outside of the walls
 Burtha.setStayInScreen(true)
 // Creates a sprite for the ball, and sets it as a kind projectile
-Ball = sprites.createProjectileFromSprite(img`
+Ball2 = sprites.createProjectileFromSprite(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . c c c c . . . . . . 
     . . . . c c c 5 5 c c c . . . . 
@@ -30,12 +125,21 @@ Ball = sprites.createProjectileFromSprite(img`
     . . . . . . c c c c . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, Burtha, 50, 50)
-Ball.setPosition(70, 90)
+// Sets ball to a x70 y90 position on start.
+Ball2.setPosition(70, 90)
 // Makes it so that the ball bounces on the wall when it touches them
-Ball.setBounceOnWall(true)
+Ball2.setBounceOnWall(true)
+// sets the score variable 
+info.setScore(0)
+// calls the function spawnBlocks, which will spawn blocks for the game.
+spawnBlocks(1, list, list, list)
 forever(function () {
     // if ball goes under x119 of the bottom wall, then game over lose happens
-    if (Ball.bottom > 119) {
+    if (Ball2.bottom > 119) {
         game.gameOver(false)
+    }
+    // if score is equal to 20, then game over win happens
+    if (info.score() == 20) {
+        game.gameOver(true)
     }
 })
